7c75b750788688603c8e471f33c123feec83308d
Tama Waddell <me@tama.id.au>
2020-04-09 17:40:13 +1000

    day 3 and 4

---
+++ Day4.hs
@@ -0,0 +1,33 @@
+import Data.List
+
+input :: [Int]
+input = [197487..673251]
+
+part1 :: Int
+part1 = length $ findpass input
+
+findpass :: [Int] -> [Int]
+findpass s = filter match s
+
+match :: Int -> Bool
+match n = neverDec digits && hasAdj digits && isSixDigits n
+    where 
+        digits = getDigits n
+
+isSixDigits :: Int -> Bool
+isSixDigits n = n >= 100000 && n < 1000000
+
+
+getDigits :: Int -> [Int]
+getDigits num = [num `div` 10^p `rem` 10 | p <- [5,4..0]]
+
+hasAdj :: [Int] -> Bool
+hasAdj = not . null . filter ((== 2) . length) . group
+
+neverDec :: [Int] -> Bool
+neverDec (x:y:rest) = x <= y && neverDec (y:rest)
+neverDec [x] = True
+neverDec [] = True
+
+pairs :: [a] -> [(a, a)]
+pairs l = zip l (drop 1 l)
