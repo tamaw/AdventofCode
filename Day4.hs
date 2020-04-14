b7e91a74198efe716d9c1c767ecea21b67b40866
Tama Waddell <me@tama.id.au>
2020-04-14 20:31:16 +1000

    small change

--- Day4.hs
+++ Day4.hs
@@ -22,12 +22,9 @@ getDigits :: Int -> [Int]
 getDigits num = [num `div` 10^p `rem` 10 | p <- [5,4..0]]
 
 hasAdj :: [Int] -> Bool
-hasAdj = not . null . filter ((== 2) . length) . group
+hasAdj = not . any ((== 2) . length) . group
 
 neverDec :: [Int] -> Bool
 neverDec (x:y:rest) = x <= y && neverDec (y:rest)
 neverDec [x] = True
 neverDec [] = True
-
-pairs :: [a] -> [(a, a)]
-pairs l = zip l (drop 1 l)
