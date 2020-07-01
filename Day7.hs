3faae9a49382eea193a07bba09d6420bdbec2778
Tama Waddell <me@tama.id.au>
2020-07-01 21:32:54 +1000

    finished day 7

--- Day7.hs
+++ Day7.hs
@@ -6,24 +6,6 @@ type Mem = [Int]
 type Outputs = [Int]
 type Inputs = [Int]
 
-initialMem :: Mem
-initialMem = [3,8,1001,8,10,8,105,1,0,0,21,42,67,76,89,110,191,272,353,434,99999,3,9,102,2,9,9,1001,9,2,9,1002,9,2,9,1001,9,2,9,4,9,99,3,9,1001,9,4,9,102,4,9,9,101,3,9,9,1002,9,2,9,1001,9,4,9,4,9,99,3,9,102,5,9,9,4,9,99,3,9,1001,9,3,9,1002,9,3,9,4,9,99,3,9,102,3,9,9,101,2,9,9,1002,9,3,9,101,5,9,9,4,9,99,3,9,1001,9,2,9,4,9,3,9,102,2,9,9,4,9,3,9,1001,9,1,9,4,9,3,9,102,2,9,9,4,9,3,9,1001,9,1,9,4,9,3,9,102,2,9,9,4,9,3,9,1002,9,2,9,4,9,3,9,1002,9,2,9,4,9,3,9,1002,9,2,9,4,9,3,9,1002,9,2,9,4,9,99,3,9,102,2,9,9,4,9,3,9,101,1,9,9,4,9,3,9,101,1,9,9,4,9,3,9,102,2,9,9,4,9,3,9,1002,9,2,9,4,9,3,9,1002,9,2,9,4,9,3,9,101,2,9,9,4,9,3,9,101,1,9,9,4,9,3,9,101,2,9,9,4,9,3,9,101,2,9,9,4,9,99,3,9,102,2,9,9,4,9,3,9,102,2,9,9,4,9,3,9,1002,9,2,9,4,9,3,9,102,2,9,9,4,9,3,9,101,1,9,9,4,9,3,9,102,2,9,9,4,9,3,9,101,2,9,9,4,9,3,9,101,2,9,9,4,9,3,9,1001,9,1,9,4,9,3,9,101,2,9,9,4,9,99,3,9,102,2,9,9,4,9,3,9,1001,9,2,9,4,9,3,9,1001,9,1,9,4,9,3,9,101,1,9,9,4,9,3,9,102,2,9,9,4,9,3,9,101,2,9,9,4,9,3,9,101,2,9,9,4,9,3,9,1002,9,2,9,4,9,3,9,101,1,9,9,4,9,3,9,1002,9,2,9,4,9,99,3,9,1002,9,2,9,4,9,3,9,1001,9,1,9,4,9,3,9,1002,9,2,9,4,9,3,9,1001,9,1,9,4,9,3,9,102,2,9,9,4,9,3,9,102,2,9,9,4,9,3,9,1001,9,1,9,4,9,3,9,1001,9,2,9,4,9,3,9,1001,9,1,9,4,9,3,9,101,2,9,9,4,9,99]
-
-phaseSettings :: [[Int]]
-phaseSettings = permutations [0..4]
-
-runPhases :: Int
-runPhases = maximum $ map runPhase phaseSettings
-
-runPhase :: [Int] -> Int
-runPhase [a, b, c, d, e]  = oe
-    where 
-        (_, [oa]) = run [a, 0] 
-        (_, [ob]) = run [b, oa]
-        (_, [oc]) = run [c, ob]
-        (_, [od]) = run [d, oc]
-        (_, [oe]) = run [e, od]
-
 data Opcode
     = NOP
     | Add
@@ -41,17 +23,93 @@ type Argument = (Mode, Int)
 halted :: Int
 halted = 99
 
-data Result
-  = Done  State
-  | Yield Int State
-  | Await State
+data StepResult
+    = Done (Mem, Outputs)
+    | NeedsMoreInput Outputs (Inputs -> StepResult)
 
-step :: PC -> Mem -> Inputs -> Outputs -> (Mem, Outputs)
-step pc mem inputs outputs = if ins == halted then (mem, outputs) else 
+showSR :: StepResult -> String
+showSR (NeedsMoreInput _ _) = "NeedsMoreInput"
+showSR (Done _) = "Done"
+
+instance Show StepResult where
+    show = showSR
+
+initialMem :: Mem
+--initialMem  = [3,26,1001,26,-4,26,3,27,1002,27,2,27,1,27,26, 27,4,27,1001,28,-1,28,1005,28,6,99,0,0,5]
+initialMem = [3,8,1001,8,10,8,105,1,0,0,21,42,67,76,89,110,191,272,353,434,99999,3,9,102,2,9,9,1001,9,2,9,1002,9,2,9,1001,9,2,9,4,9,99,3,9,1001,9,4,9,102,4,9,9,101,3,9,9,1002,9,2,9,1001,9,4,9,4,9,99,3,9,102,5,9,9,4,9,99,3,9,1001,9,3,9,1002,9,3,9,4,9,99,3,9,102,3,9,9,101,2,9,9,1002,9,3,9,101,5,9,9,4,9,99,3,9,1001,9,2,9,4,9,3,9,102,2,9,9,4,9,3,9,1001,9,1,9,4,9,3,9,102,2,9,9,4,9,3,9,1001,9,1,9,4,9,3,9,102,2,9,9,4,9,3,9,1002,9,2,9,4,9,3,9,1002,9,2,9,4,9,3,9,1002,9,2,9,4,9,3,9,1002,9,2,9,4,9,99,3,9,102,2,9,9,4,9,3,9,101,1,9,9,4,9,3,9,101,1,9,9,4,9,3,9,102,2,9,9,4,9,3,9,1002,9,2,9,4,9,3,9,1002,9,2,9,4,9,3,9,101,2,9,9,4,9,3,9,101,1,9,9,4,9,3,9,101,2,9,9,4,9,3,9,101,2,9,9,4,9,99,3,9,102,2,9,9,4,9,3,9,102,2,9,9,4,9,3,9,1002,9,2,9,4,9,3,9,102,2,9,9,4,9,3,9,101,1,9,9,4,9,3,9,102,2,9,9,4,9,3,9,101,2,9,9,4,9,3,9,101,2,9,9,4,9,3,9,1001,9,1,9,4,9,3,9,101,2,9,9,4,9,99,3,9,102,2,9,9,4,9,3,9,1001,9,2,9,4,9,3,9,1001,9,1,9,4,9,3,9,101,1,9,9,4,9,3,9,102,2,9,9,4,9,3,9,101,2,9,9,4,9,3,9,101,2,9,9,4,9,3,9,1002,9,2,9,4,9,3,9,101,1,9,9,4,9,3,9,1002,9,2,9,4,9,99,3,9,1002,9,2,9,4,9,3,9,1001,9,1,9,4,9,3,9,1002,9,2,9,4,9,3,9,1001,9,1,9,4,9,3,9,102,2,9,9,4,9,3,9,102,2,9,9,4,9,3,9,1001,9,1,9,4,9,3,9,1001,9,2,9,4,9,3,9,1001,9,1,9,4,9,3,9,101,2,9,9,4,9,99]
+
+phaseSettings :: [[Int]]
+phaseSettings = permutations [5..9]
+
+runPhase :: [Int] -> Int
+runPhase [a, b, c, d, e]  = oe
+    where 
+        (_, [oa]) = run [a, 0] 
+        (_, [ob]) = run [b, oa]
+        (_, [oc]) = run [c, ob]
+        (_, [od]) = run [d, oc]
+        (_, [oe]) = run [e, od]
+
+runPhases2 :: [Int]
+runPhases2 = maximum $ map runPhase2 phaseSettings
+
+runPhase2 :: [Int] -> Outputs
+runPhase2 [a, b, c, d, e]  = loop ka0 kb0 kc0 kd0 ke0 [0]
+    where 
+        NeedsMoreInput _ ka0 = initializeWithPhase a
+        NeedsMoreInput _ kb0 = initializeWithPhase b
+        NeedsMoreInput _ kc0 = initializeWithPhase c
+        NeedsMoreInput _ kd0 = initializeWithPhase d
+        NeedsMoreInput _ ke0 = initializeWithPhase e
+
+        loop :: (Inputs -> StepResult)
+             -> (Inputs -> StepResult)
+             -> (Inputs -> StepResult)
+             -> (Inputs -> StepResult)
+             -> (Inputs -> StepResult)
+             -> Inputs
+             -> Outputs
+        loop ka kb kc kd ke input
+            | NeedsMoreInput oa ka' <- traceShowId $ ka input
+            , NeedsMoreInput ob kb' <- traceShowId $ kb oa
+            , NeedsMoreInput oc kc' <- traceShowId $ kc ob
+            , NeedsMoreInput od kd' <- traceShowId $ kd oc
+            , NeedsMoreInput oe ke' <- traceShowId $ ke od
+                = loop ka' kb' kc' kd' ke' oe
+        loop ka kb kc kd ke input
+            | oa <- outputs (ka input)
+            , ob <- outputs (kb oa)
+            , oc <- outputs (kc ob)
+            , od <- outputs (kd oc)
+            , oe <- outputs (ke od)
+                = oe
+
+        outputs :: StepResult -> Outputs
+        outputs (Done (_, o)) = o
+        outputs (NeedsMoreInput o _) = o
+
+        initializeWithPhase :: Int -> StepResult
+        initializeWithPhase phase =
+                case runIncremental of
+                    NeedsMoreInput _ k -> k [phase]
+                    Done _ -> error "unexpected state"
+
+run :: Inputs -> (Mem, Outputs)
+run inputs = case step 0 initialMem inputs [] of
+        Done r -> r
+        NeedsMoreInput _ _ -> error "ran out of inputs"
+
+runIncremental :: StepResult
+runIncremental = step 0 initialMem [] []
+
+step :: PC -> Mem -> Inputs -> Outputs -> StepResult
+step pc mem inputs outputs = if ins == halted then Done (mem, outputs) else 
      case opcode of
             Add         -> step (pc+4)      add      inputs outputs
             Multiply    -> step (pc+4)      multiply inputs outputs
-            Input       -> step (pc+2)      mem'     inputs' outputs
+            Input       -> if null inputs 
+                            then NeedsMoreInput outputs (\newInput -> step pc mem newInput []) 
+                            else step (pc+2) mem' inputs' outputs
             Output      -> step (pc+2)      mem      inputs output
             JumpIfTrue  -> step jumpIfTrue  mem      inputs outputs
             JumpIfFalse -> step jumpIfFalse mem      inputs outputs
@@ -63,7 +121,7 @@ step pc mem inputs outputs = if ins == halted then (mem, outputs) else
         a = mem !! (pc + 1)
         b = mem !! (pc + 2)
         c = mem !! (pc + 3)
-        (opcode, (ma,mb,mc)) = traceShowId $ decode ins
+        (opcode, (ma,mb,mc)) = decode ins
         add = operation (+) paramA paramB paramC mem
         multiply = operation (*) paramA paramB paramC mem
         (inputs', mem') = input' a (inputs, mem)
@@ -104,6 +162,4 @@ store (a, v) mem = output
           before = take a mem
           after = drop (a+1) mem
 
-run :: Inputs -> (Mem, Outputs)
-run inputs = step 0 initialMem inputs []
 
