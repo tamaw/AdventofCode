8ccbac5a5610af03ff27057332c53b07e4ac0b30
Tama Waddell <me@tama.id.au>
2023-12-03 18:56:30 +1100

    day1 part 2

--- 2023/day1.c
+++ 2023/day1.c
@@ -1,14 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #define BUFF_SIZE 100
 #define MAX_NUMS 1000
 
-int main(void)
+int part1(FILE* fp, int* nums)
 {
-  FILE* fp = fopen("day1input.txt", "r");
   char* line = malloc(BUFF_SIZE * sizeof(char));
-  int nums[MAX_NUMS];
-  int sum = 0;
 
   int n;
   for(n = 0; (line = fgets(line, BUFF_SIZE, fp)); n++) {
@@ -26,8 +24,77 @@ int main(void)
 
     char* nvalue = (char[]){n0, n1, '\0'};
     nums[n] = atoi(nvalue);
-    printf("n0: %c, n1: %c, n: %d\n", n0, n1, nums[n]);
   }
+
+  return n;
+}
+
+int part2(FILE* fp, int* nums)
+{
+  const char* words[] = {"one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+  int wordLengths[9];
+  char* line = malloc(BUFF_SIZE * sizeof(char));
+
+  // precompute word legnths
+  for(int w = 0; w < sizeof(words) / sizeof(words[0]); w++)
+  {
+    wordLengths[w] = strlen(words[w]);
+  }
+
+  int n;
+  for(n = 0; (line = fgets(line, BUFF_SIZE, fp)); n++) {
+    int letterCount[] = {0,0,0,0,0,0,0,0,0,0};
+    int n0 = -1, n1 = -1;
+    char c;
+
+    for(int i = 0; (c = line[i]) != '\n'; i++) {
+      char c = line[i];
+
+      // number
+      if(c >= '0' && c <= '9') {
+        if(n0 == -1) n0 = c;
+        else n1 = c;
+      }
+
+      // word search
+      for(int j = 0; j < 9; j++) {
+
+        if(c != words[j][letterCount[j]]) {
+          letterCount[j] = 0;
+        }
+
+        if(c == words[j][letterCount[j]]) {
+          if(++letterCount[j] == wordLengths[j]) // match
+          {
+            if(n0 == -1) n0 = j+'1';
+            else n1 = j+'1';
+            letterCount[j] = 0;
+          }
+        }
+
+      }
+    }
+
+     // missing second value
+     if(n1 == -1) n1 = n0;
+
+     // concat and convert
+     char* nvalue = (char[]){n0, n1, '\0'};
+     nums[n] = atoi(nvalue);
+
+  }
+
+  return n;
+}
+
+int main(void)
+{
+  FILE* fp = fopen("day1input.txt", "r");
+  int nums[MAX_NUMS];
+  int sum = 0;
+
+  // int n = part1(fp, nums);
+  int n = part2(fp, nums);
 
   for(int i = 0; i < n; i++)
     sum += nums[i];
